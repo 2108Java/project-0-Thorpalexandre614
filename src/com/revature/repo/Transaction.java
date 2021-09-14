8f250ef2c44dd967c0536e5f1fdc474a62104f94
Alexandre Thorp <Thorpalexandre614@gmail.com>
2021-09-14 15:43:05 -0400

    More work on DAO

--- src/com/revature/repo/Transaction.java
+++ src/com/revature/repo/Transaction.java
@@ -44,7 +44,7 @@ public class Transaction implements TransactionInterface{
 			
 			String inputQuery = "INSERT INTO transaction_log(transaction_type, transfer_origin, "
 					+ "transfer_target, balance_origin, balance_target, approved) "
-					+ "VALUES('withdrawl', ?, ?, ?, ?, false)";
+					+ "VALUES('withdrawl', ?, ?, ?, ?, true)";
 			
 			PreparedStatement ps2 = connection.prepareStatement(inputQuery);
 			
@@ -82,7 +82,67 @@ public class Transaction implements TransactionInterface{
 	
 	public boolean deposit(int amount, String pin) {
 		
-		return false;
+		boolean status1 = false;
+		boolean status2 = false;
+		boolean status3 = false;
+		boolean statusFinal = false;
+		
+		try(Connection connection = DriverManager.getConnection(url, username, password)){
+			
+			String balanceQuery = "SELECT balance FROM user_data WHERE pin = ?";
+			
+			PreparedStatement ps1 = connection.prepareStatement(balanceQuery);
+
+			ps1.setString(1, pin);
+			
+			
+			ResultSet rs = ps1.executeQuery();
+			
+			status1 = true;
+			
+			int balance = 0;
+			int newBalance = 0;
+			
+			while(rs.next()) {
+				 balance = rs.getInt("balance");
+				 newBalance = (balance + amount);
+			}
+			
+			String inputQuery = "INSERT INTO transaction_log(transaction_type, transfer_origin, "
+					+ "transfer_target, balance_origin, balance_target, approved) "
+					+ "VALUES('deposit', ?, ?, ?, ?, true)";
+			
+			PreparedStatement ps2 = connection.prepareStatement(inputQuery);
+			
+			ps2.setString(1, pin);
+			ps2.setString(2, pin);
+			ps2.setInt(3, balance);
+			ps2.setInt(4, newBalance);
+			
+			ps2.execute();
+			
+			status2 = true;
+			
+			String balanceUpdate = "UPDATE user_data SET balance = ? WHERE pin = ? ";
+			
+			PreparedStatement ps3 = connection.prepareStatement(balanceUpdate);
+			
+			ps3.setInt(1, newBalance);
+			ps3.setString(2, pin);
+			
+			ps3.execute();
+			
+			status3 = true;
+			
+			if(status1 && status2 && status3) {
+				statusFinal = true;
+			}
+			
+		} catch(SQLException e) {
+			e.printStackTrace();
+		}
+		
+		return statusFinal;
 	}
 
 	
